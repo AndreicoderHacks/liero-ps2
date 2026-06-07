21f07acade1ad5097b820e6acd52d0303b0f3b81
PSART <144950784+AndreicoderHacks@users.noreply.github.com>
2026-06-07 23:21:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -92,14 +92,9 @@ void game_tick(GameState *g) {
 int main(void) {
     SifInitRpc(0);
 
-    dmaKit_init(D_CTRL_RELE_OFF, D_CTRL_MFD_OFF, D_CTRL_STS_UNSPEC,
-                D_CTRL_STD_OFF, D_CTRL_RCYC_8, 1 << DMA_CHANNEL_GIF);
-    dmaKit_chan_init(DMA_CHANNEL_GIF);
-
+    // Exact secventa din Minicraft PS2 care functioneaza
     GSGLOBAL *gsGlobal        = gsKit_init_global();
     gsGlobal->Mode            = GS_MODE_NTSC;
-    gsGlobal->Interlace       = GS_INTERLACED;
-    gsGlobal->Field           = GS_FIELD;
     gsGlobal->Width           = SCREEN_W;
     gsGlobal->Height          = SCREEN_H;
     gsGlobal->PSM             = GS_PSM_CT32;
@@ -108,8 +103,12 @@ int main(void) {
     gsGlobal->DoubleBuffering = GS_SETTING_ON;
     gsGlobal->PrimAlphaEnable = GS_SETTING_OFF;
 
+    dmaKit_init(D_CTRL_RELE_OFF, D_CTRL_MFD_OFF, D_CTRL_STS_UNSPEC,
+                D_CTRL_STD_OFF, D_CTRL_RCYC_8, 1 << DMA_CHANNEL_GIF);
+    dmaKit_chan_init(DMA_CHANNEL_GIF);
+
     gsKit_init_screen(gsGlobal);
-    gsKit_mode_switch(gsGlobal, GS_PERSISTENT);
+    gsKit_mode_switch(gsGlobal, GS_ONESHOT);
 
     input_init();
     sfx_init();
